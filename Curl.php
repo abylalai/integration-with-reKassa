f030e38f64c7c9dc37caf9dd1db4eaeb88105507
Abylai Khan <63773374+abylalai@users.noreply.github.com>
2022-07-27 10:27:33 +0600

    Update Curl.php

--- Curl.php
+++ Curl.php
@@ -1,5 +1,5 @@
 <?
-class Curl{
+class Curl extends CurlClient{
     
     public $response = null;
     public $http_code = null;
@@ -9,20 +9,6 @@ class Curl{
 
     public $options = [];
 
-    private $maxRedirs = 10;
-    private $timeout = 30;
-    private $curlHttpVersion = 'CURL_HTTP_VERSION_1_1';
-
-    private function doRequest(){
-        $curl = curl_init($this->url);
-        $this->setDefaultOptions();
-        curl_setopt_array($curl, $this->options);
-
-        $this->response = curl_exec($curl);
-        $this->http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        $this->error = curl_error($curl);
-        curl_close($curl);
-    }
 
     public function POST($url = '', $header = '', $data = ''){
         $this->setUrl($url);
@@ -58,12 +44,23 @@ class Curl{
     public function PATCH($url = '', $header = ''){
         
     }
+}
 
-    private function setDefaultOptions(){
-        $this->setOpt(CURLOPT_RETURNTRANSFER, TRUE);
-        $this->setOpt(CURLOPT_MAXREDIRS, $this->maxRedirs);
-        $this->setOpt(CURLOPT_TIMEOUT, $this->timeout);
-        $this->setOpt(CURLOPT_HTTP_VERSION, $this->curlHttpVersion);
+class CurlClient{
+
+    public $maxRedirs = 10;
+    public $timeout = 30;
+    public $curlHttpVersion = 'CURL_HTTP_VERSION_1_1';
+
+    protected function doRequest(){
+        $curl = curl_init($this->url);
+        $this->setDefaultOptions();
+        curl_setopt_array($curl, $this->options);
+
+        $this->response = curl_exec($curl);
+        $this->http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $this->error = curl_error($curl);
+        curl_close($curl);
     }
 
     public function setOpt($option_name, $option_value){
@@ -71,11 +68,16 @@ class Curl{
         return $this;
     }
 
-    private function setUrl($url){
+    protected function setDefaultOptions(){
+        $this->setOpt(CURLOPT_RETURNTRANSFER, TRUE);
+        $this->setOpt(CURLOPT_MAXREDIRS, $this->maxRedirs);
+        $this->setOpt(CURLOPT_TIMEOUT, $this->timeout);
+        $this->setOpt(CURLOPT_HTTP_VERSION, $this->curlHttpVersion);
+    }
+
+    protected function setUrl($url){
         $this->url = $url;
         return $this;
     }
-
 }
-
 ?>
