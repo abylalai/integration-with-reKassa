a7f8075aa5dc44ceb4e944ced7cbbaacfffee604
Abylai Khan <63773374+abylalai@users.noreply.github.com>
2022-08-05 10:29:22 +0600

    Update cron_robot.php

--- cron_robot.php
+++ cron_robot.php
@@ -1,8 +1,9 @@
 <?
+echo ('<p>[' . date('d.m.Y H:i:s') .'] start</p>' . "\n");
 date_default_timezone_set('Asia/Almaty');
 if (
-	(date("G") >= 7 && date("G") <= 9)			// С 06:00 до 09:00
-	//|| date("Y-m-d") == '2022-05-12'
+	//(date("G") >= 3 && date("G") <= 6)			// С 03:00 до 06:00
+	true
 )
 {
 	$lang="ru";
@@ -12,8 +13,6 @@ if (
 	include_once($_SERVER["DOCUMENT_ROOT"].'/general/mysql.php');
     include_once($_SERVER["DOCUMENT_ROOT"].'/general/api.php');
     // CLASS REKASSA =====================
-    include_once($_SERVER["DOCUMENT_ROOT"].'/rekassa/abylai/RekassaAPI.php'); # апи реккассы
-    include_once($_SERVER["DOCUMENT_ROOT"].'/rekassa/abylai/Integration.php'); # интеграция рекассы с нашем сайтом
     include_once($_SERVER["DOCUMENT_ROOT"].'/rekassa/abylai/REKASSA.php'); # класс мост между апи и интеграцией
 	
 	ini_set("max_execution_time", "900");
@@ -34,7 +33,7 @@ if (
 	
 	if ($stop == 1)
 	{
-		$limitH = 15;
+		$limitH = 3;
 		$s=$mysqli->query("SELECT * FROM `i_rekassa_info` WHERE `check_date` is null ORDER BY `id` ASC LIMIT ".$limitH);		
 		if ($s->num_rows>0)
 		{		
@@ -46,13 +45,10 @@ if (
                 $rekassa->Integration
                 ->setShiftPage(0) # номер страницы смены
                 ->setShiftSize(20) # кол-во смен на одном странице
-                ->setTicketPage(0) # номер страницы тикета
-                ->setTicketSize(20) # кол-во тикетов на одном странице 
-                ->setAutoTicketToNextPage(true) # автоматический переключатся на вторую страницу тикетов
                 ->setAutoShiftToNextPage(true); # автомаический переключатся на вторую страницу смен 
                                                 # (допустим выводились все записи на странице 0 и автоматический будут ввыодит следущую страницу)
                 
-                $result = $rekassa->Integration->ticketsToSQL();
+                $result = $rekassa->Integration->shiftsToSQL();
                 
                 $rekassa->Integration->debug($result);
                 $upd = $mysqli->query('UPDATE `i_rekassa_info` SET `check_date` = \'1\' WHERE `id_user`=\''.$id_user.'\'');
@@ -63,9 +59,12 @@ if (
 			// СТОП
 			$sql_updateD = "UPDATE `i_cron` SET `active`=0, `check_end`=1, `check_date`='".date("Y-m-d")."' WHERE `id`='".$id_cron."'";
 			$updateD = $mysqli->query($sql_updateD);
-            $upd = $mysqli->query('UPDATE `i_rekassa_info` SET `check_date` = \'0\' WHERE `check_date` = 1');
+            $upd = $mysqli->query('UPDATE `i_rekassa_info` SET `check_date` = NULL WHERE `check_date` = 1');
 			echo 'End today!';
 		}
 	}
 }
+
+echo ('<p>[' . date('d.m.Y H:i:s') .'] end</p>' . "\n");
+
 ?>
